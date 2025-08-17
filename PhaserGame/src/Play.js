b3d9fe9c48b63814ee00a76653509daeba6148e4
Hunter Roberts <hrob997@aucklanduni.ac.nz>
2025-08-18 09:43:55 +1200

    refactor: remove legacy card game logic from Play.js

--- PhaserGame/src/Play.js
+++ PhaserGame/src/Play.js
@@ -1,13 +1,10 @@
-import { createCard } from './createCard';
 import Phaser from 'phaser';
 
 /**
- * Card Memory Game by Francisco Pereira (Gammafp)
+ * Maths Card Game
  * -----------------------------------------------
  *
- * Test your memory skills in this classic game of matching pairs.
- * Flip over cards to reveal pictures, and try to remember where each image is located.
- * Match all the pairs to win!
+ * Test your math skills in this fun and challenging game!
  *
  * Music credits:
  * "Fat Caps" by Audionautix is licensed under the Creative Commons Attribution 4.0 license. https://creativecommons.org/licenses/by/4.0/
@@ -20,22 +17,9 @@ export class Play extends Phaser.Scene
     // Cards Game Objects
     cards = [];
 
-    // History of card opened
-    cardOpened = undefined;
-
-    // Can play the game
-    canMove = false;
-
     // Game variables
     lives = 0;
 
-    // Grid configuration
-    gridConfiguration = {
-        x: 113,
-        y: 102,
-        paddingX: 10,
-        paddingY: 10
-    }
 
     constructor ()
     {
@@ -48,17 +32,14 @@ export class Play extends Phaser.Scene
     {
         // Fadein camera
         this.cameras.main.fadeIn(500);
-        this.lives = 10;
         this.volumeButton();
     }
 
     create ()
     {
-        // Background image
-        this.add.image(this.gridConfiguration.x - 63, this.gridConfiguration.y - 77, "background").setOrigin(0);
-
+        //prob belongs in ui
         const titleText = this.add.text(this.sys.game.scale.width / 2, this.sys.game.scale.height / 2,
-            "Memory Card Game\nClick to Play",
+            "Maths Card Game\nClick to Play",
             { align: "center", strokeThickness: 4, fontSize: 40, fontStyle: "bold", color: "#8c7ae6" }
         )
             .setOrigin(.5)
@@ -101,72 +82,8 @@ export class Play extends Phaser.Scene
 
     restartGame ()
     {
-        this.cardOpened = undefined;
-        this.cameras.main.fadeOut(200 * this.cards.length);
-        this.cards.reverse().map((card, index) => {
-            this.add.tween({
-                targets: card.gameObject,
-                duration: 500,
-                y: 1000,
-                delay: index * 100,
-                onComplete: () => {
-                    card.gameObject.destroy();
-                }
-            })
-        });
-
-        this.time.addEvent({
-            delay: 200 * this.cards.length,
-            callback: () => {
-                this.cards = [];
-                this.canMove = false;
-                this.scene.restart();
-                this.sound.play("card-slide", { volume: 1.2 });
-            }
-        })
+        //TODO implement  method
     }
-
-    createGridCards ()
-    {
-        // Phaser random array position
-        const gridCardNames = Phaser.Utils.Array.Shuffle([...this.cardNames, ...this.cardNames]);
-
-        return gridCardNames.map((name, index) => {
-            const newCard = createCard({
-                scene: this,
-                x: this.gridConfiguration.x + (98 + this.gridConfiguration.paddingX) * (index % 4),
-                y: -1000,
-                frontTexture: name,
-                cardName: name
-            });
-            this.add.tween({
-                targets: newCard.gameObject,
-                duration: 800,
-                delay: index * 100,
-                onStart: () => this.sound.play("card-slide", { volume: 1.2 }),
-                y: this.gridConfiguration.y + (128 + this.gridConfiguration.paddingY) * Math.floor(index / 4)
-            })
-            return newCard;
-        });
-    }
-
-    createHearts ()
-    {
-        return Array.from(new Array(this.lives)).map((el, index) => {
-            const heart = this.add.image(this.sys.game.scale.width + 1000, 20, "heart")
-                .setScale(2)
-
-            this.add.tween({
-                targets: heart,
-                ease: Phaser.Math.Easing.Expo.InOut,
-                duration: 1000,
-                delay: 1000 + index * 200,
-                x: 140 + 30 * index // marginLeft + spaceBetween * index
-            });
-            return heart;
-        });
-    }
-
 
     volumeButton ()
     {
@@ -197,8 +114,8 @@ export class Play extends Phaser.Scene
         });
     }
 
-    startGame ()
-    {
+    startGame () {
+        // Launch the UI layout when the game starts
 
         // WinnerText and GameOverText
         const winnerText = this.add.text(this.sys.game.scale.width / 2, -1000, "YOU WIN",
@@ -216,173 +133,10 @@ export class Play extends Phaser.Scene
             .setOrigin(.5)
             .setInteractive();
 
-        // Start lifes images
-        const hearts = this.createHearts();
 
-        // Create a grid of cards
-        this.cards = this.createGridCards();
-
-        // Start canMove
-        this.time.addEvent({
-            delay: 200 * this.cards.length,
-            callback: () => {
-                this.canMove = true;
-            }
-        });
+        // Start Events
 
         // Game Logic
-        this.input.on(Phaser.Input.Events.POINTER_MOVE, (pointer) => {
-            if (this.canMove) {
-                const card = this.cards.find(card => card.gameObject.hasFaceAt(pointer.x, pointer.y));
-                if (card) {
-                    this.input.setDefaultCursor("pointer");
-                } else {
-                    if(go[0]) {
-                        if(go[0].name !== "volume-icon") {
-                            this.input.setDefaultCursor("pointer");
-                        }
-                    } else {
-                        this.input.setDefaultCursor("default");
-                    }
-                }
-            }
-        });
-        this.input.on(Phaser.Input.Events.POINTER_DOWN, (pointer) => {
-            if (this.canMove && this.cards.length) {
-                const card = this.cards.find(card => card.gameObject.hasFaceAt(pointer.x, pointer.y));
-
-                if (card) {
-                    this.canMove = false;
-
-                    // Detect if there is a card opened
-                    if (this.cardOpened !== undefined) {
-                        // If the card is the same that the opened not do anything
-                        if (this.cardOpened.gameObject.x === card.gameObject.x && this.cardOpened.gameObject.y === card.gameObject.y) {
-                            this.canMove = true;
-                            return false;
-                        }
-
-                        card.flip(() => {
-                            if (this.cardOpened.cardName === card.cardName) {
-                                // ------- Match -------
-                                this.sound.play("card-match");
-                                // Destroy card selected and card opened from history
-                                this.cardOpened.destroy();
-                                card.destroy();
-
-                                // remove card destroyed from array
-                                this.cards = this.cards.filter(cardLocal => cardLocal.cardName !== card.cardName);
-                                // reset history card opened
-                                this.cardOpened = undefined;
-                                this.canMove = true;
-
-                            } else {
-                                // ------- No match -------
-                                this.sound.play("card-mismatch");
-                                this.cameras.main.shake(600, 0.01);
-                                // remove life and heart
-                                const lastHeart = hearts[hearts.length - 1];
-                                this.add.tween({
-                                    targets: lastHeart,
-                                    ease: Phaser.Math.Easing.Expo.InOut,
-                                    duration: 1000,
-                                    y: - 1000,
-                                    onComplete: () => {
-                                        lastHeart.destroy();
-                                        hearts.pop();
-                                    }
-                                });
-                                this.lives -= 1;
-                                // Flip last card selected and flip the card opened from history and reset history
-                                card.flip();
-                                this.cardOpened.flip(() => {
-                                    this.cardOpened = undefined;
-                                    this.canMove = true;
-
-                                });
-                            }
-
-                            // Check if the game is over
-                            if (this.lives === 0) {
-                                // Show Game Over text
-                                this.sound.play("whoosh", { volume: 1.3 });
-                                this.add.tween({
-                                    targets: gameOverText,
-                                    ease: Phaser.Math.Easing.Bounce.Out,
-                                    y: this.sys.game.scale.height / 2,
-                                });
-
-                                this.canMove = false;
-                            }
-
-                            // Check if the game is won
-                            if (this.cards.length === 0) {
-                                this.sound.play("whoosh", { volume: 1.3 });
-                                this.sound.play("victory");
-
-                                this.add.tween({
-                                    targets: winnerText,
-                                    ease: Phaser.Math.Easing.Bounce.Out,
-                                    y: this.sys.game.scale.height / 2,
-                                });
-                                this.canMove = false;
-                            }
-                        });
-
-                    } else if (this.cardOpened === undefined && this.lives > 0 && this.cards.length > 0) {
-                        // If there is not a card opened save the card selected
-                        card.flip(() => {
-                            this.canMove = true;
-                        });
-                        this.cardOpened = card;
-                    }
-                }
-            }
-
-        });
-
-
-        // Text events
-        winnerText.on(Phaser.Input.Events.POINTER_OVER, () => {
-            winnerText.setColor("#FF7F50");
-            this.input.setDefaultCursor("pointer");
-        });
-        winnerText.on(Phaser.Input.Events.POINTER_OUT, () => {
-            winnerText.setColor("#8c7ae6");
-            this.input.setDefaultCursor("default");
-        });
-        winnerText.on(Phaser.Input.Events.POINTER_DOWN, () => {
-            this.sound.play("whoosh", { volume: 1.3 });
-            this.add.tween({
-                targets: winnerText,
-                ease: Phaser.Math.Easing.Bounce.InOut,
-                y: -1000,
-                onComplete: () => {
-                    this.restartGame();
-                }
-            })
-        });
-
-        gameOverText.on(Phaser.Input.Events.POINTER_OVER, () => {
-            gameOverText.setColor("#FF7F50");
-            this.input.setDefaultCursor("pointer");
-        });
-
-        gameOverText.on(Phaser.Input.Events.POINTER_OUT, () => {
-            gameOverText.setColor("#8c7ae6");
-            this.input.setDefaultCursor("default");
-        });
-
-        gameOverText.on(Phaser.Input.Events.POINTER_DOWN, () => {
-            this.add.tween({
-                targets: gameOverText,
-                ease: Phaser.Math.Easing.Bounce.InOut,
-                y: -1000,
-                onComplete: () => {
-                    this.restartGame();
-                }
-            })
-        });
     }
 
 }
